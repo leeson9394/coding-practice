bd4874c787c1b7da58d14e030ba76603a2fb837d
Rijutady <rijutady@gmail.com>
2021-02-23 14:59:12 -0800

    battleship problem

---
+++ Facebook/battleship.py
@@ -0,0 +1,25 @@
+# leetcode 419. Battleships in a Board
+# https://leetcode.com/problems/battleships-in-a-board/
+
+class Solution(object):
+    def countBattleships(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: int
+        """
+        row = len(board)
+        col = len(board[0])
+        count = 0
+        for i in range(row):
+            for j in range(col):
+                if board[i][j] == 'X':
+                    if (i>=1 and board[i-1][j] == 'X') or (j>=1 and board[i][j-1] == 'X'):
+                        continue
+                    count += 1
+        return count
+        
+board = [["X",".",".","X"],[".",".",".","X"],[".",".",".","X"]]
+
+sol = Solution()
+res = sol.countBattleships(board)
+print(res)
