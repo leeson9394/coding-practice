072ae67ae625ce229c30c84012170e7a503a90b7
Chen Li <chen.li@appdynamics.com>
2022-03-20 12:08:34 -0700

    fix sorting

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,8 +1,8 @@
 def bubble_sort(arr):
     for i in range(len(arr)-1):
-        for j in range(i+1, len(arr)):
-            if arr[i] > arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 arr = [15,41,13,2,91]
